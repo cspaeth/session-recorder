75281b08214014615df11eb13f5f1b1b04f92829
Christian Spaeth <mail@cspaeth.de>
2020-09-03 00:53:22 +0200

    one more save state...

--- backend/ssr/consumers.py
+++ backend/ssr/consumers.py
@@ -1,4 +1,5 @@
 import logging
+from collections import OrderedDict
 from os import mkdir, path
 from shutil import copy
 from threading import Timer, RLock, Thread
@@ -16,42 +17,12 @@ import reapy.reascript_api
 
 log = logging.getLogger(__name__)
 SESSION_ROOM_NAME = 'session'
-
-CURRENT_USER = "csp"
-
-STATUS = {
-    'auth': {
-        'user': 'csp',
-    },
-}
-
-API = RLock()
-
-def reaper_access(f):
-    def inner(*args, **kwargs):
-        with API:
-            log.debug("Reaper guarded: %s " % f.__name__)
-            try:
-                return f(*args, **kwargs)
-            except (ConnectionError, DisabledDistAPIError):
-                log.exception("Error connecting to recoder, trying to reconnect")
-                args[0].on_disconnect()
-
-    return inner
-
-
-def mutation(name, data):
-    return {
-        'action': name,
-        'data': data
-    }
-
 HOST_PATH = "/Users/csp/Projects/session-recorder/var/sessions/"
 BASE_PATH = "/sessions/"
 BASE_TEMPLATE = BASE_PATH + "template.RPP"
 SESSION_FILE_NAME = "Session.RPP"
 REAPER_IP = "192.168.178.32"
-
+CURRENT_USER="Chriss"
 DISCONNECTED = {
     'state': "disconnected"  # connected, ready, recording, playing,
 }
@@ -62,61 +33,121 @@ CONNECTED = {
 
 class ApplicationState:
     def __init__(self):
-        self.state = {
-            'RECORDER_UPDATE': {},
-            'UPDATE_FULL_SESSION': {}
+        self.modules = OrderedDict()
+        self.state = {}
+
+    def all_states(self):
+        return [self.get_status_message(module) for module in self.modules.keys()]
+
+    def get_status_message(self, module_name):
+        return {
+            'action': module_name,
+            'data': self.state[module_name]
         }
 
-    def update(self, module, data):
+    def register_module(self, module):
+        print (module.name)
+        self.modules[module.name] = module
+        self.state[module.name] = module.get_module_status()
+
+    def update(self, module_name, data):
         # log.info("state update %s -> %s", module, data)
-        self.state[module] = data
+        self.state[module_name] = data
         async_to_sync(get_channel_layer().group_send)(
             SESSION_ROOM_NAME,
             {
                 'type': 'send_update',
-                'scope': module
+                'scope': module_name
              }
         )
 
+    def dispatch_action(self, action_name, data):
+        for module in self.modules.values():
+            method = getattr(module, action_name, None)
+            if method and getattr(method, 'isAction', False):
+                method(data)
 
-appState = ApplicationState()
+
+class StateModule:
+    name = None
+
+    def update_module_status(self):
+        appState.update(self.name, self.get_module_status())
 
 
-class ReaperRecorder:
+def action(f):
+    f.isAction = True
+    return f
+
+
+class reaper_access:
+    API = RLock()
+
+    def __init__(self, send_update=True):
+        self.send_update = send_update
+
+    def __call__(self, f):
+        def inner(*args, **kwargs):
+            with self.API:
+                log.debug("Reaper guarded: %s " % f.__name__)
+                try:
+                    return f(*args, **kwargs)
+                except (ConnectionError, DisabledDistAPIError):
+                    log.exception("Error connecting to recoder, trying to reconnect")
+                    args[0].on_disconnect()
+                finally:
+                    if self.send_update:
+                        args[0].update_module_status()
+        return inner
+
+
+class ReaperRecorder(StateModule):
+
+    name = 'RECORDER_UPDATE'
     def __init__(self):
         self.connected_once = False
+        self.is_connected = False
 
-    @reaper_access
+    def periodic_recorder_update(self):
+        while True:
+            sleep(.1)
+            self.update_module_status()
+
+    def on_disconnect(self):
+        self.is_connected = False
+        self.update_module_status()
+        log.warning("Disconnected - scheduling reconnect in 10 s")
+        timer = Timer(10, self.connect)
+        timer.setDaemon(True)
+        timer.start()
+
+    @reaper_access()
     def connect(self):
         if not self.connected_once:
             log.warning("Connecting to reaper (adress=%s)..." % REAPER_IP)
             reapy.connect(REAPER_IP)
             self.connected_once = True
+            Thread(target=self.periodic_recorder_update, daemon=True).start()
         else:
             log.warning("Reconnecting to reaper...")
             reapy.reconnect()
 
+        self.is_connected = True
         log.info("Connected to reaper!")
         self.on_connect()
 
-    @reaper_access
+    @reaper_access()
     def start_new_project(self, name):
         session_file_host = self._create_project_from_template(name)
-        self.open_project(session_file_host)
+        self._open_project(session_file_host)
         return session_file_host
 
-    @reaper_access
-    def open_project(self, project_file):
-        if self.get_open_project_path() != project_file:
-            self._save_current_project()
-            reapy.open_project(project_file)
-
-    @reaper_access
+    @reaper_access()
     def close_project(self):
         self._save_current_project()
         reapy.Project().close()
 
-    @reaper_access
+    @reaper_access()
     def start_recording(self):
         project = reapy.Project()
         project.perform_action(40043)  # Go to end of project
@@ -124,32 +155,32 @@ class ReaperRecorder:
         project.perform_action(1013)  # Record
         return start_position
 
-    @reaper_access
+    @reaper_access()
     def stop(self):
         project = reapy.Project()
         project.stop()
         return project.length
 
-    @reaper_access
+    @reaper_access()
     def select(self, start, length):
         project = reapy.Project()
         project.time_selection = start, start + length
         project.perform_action(40630)  # go to start of selection
 
-    @reaper_access
+    @reaper_access()
     def seek(self, position):
         reapy.Project().cursor_position = position
 
-    @reaper_access
+    @reaper_access()
     def play(self):
         reapy.Project().play()
 
-    @reaper_access
+    @reaper_access(send_update=False)
     def get_open_project_path(self):
         project = reapy.Project()
         return path.join(project.path, project.name)
 
-    @reaper_access
+    @reaper_access(send_update=False)
     def _save_current_project(self):
         current_project = reapy.Project()
         if current_project.is_dirty:
@@ -162,8 +193,8 @@ class ReaperRecorder:
         session_file_host = path.join(HOST_PATH, session_file)
         return session_file_host
 
-    @reaper_access
-    def get_status(self):
+    @reaper_access(send_update=False)
+    def get_module_status(self):
         project = reapy.Project()
         if project.name == '':
             return CONNECTED
@@ -182,120 +213,102 @@ class ReaperRecorder:
             'state': state
         }
 
-
-reaper = ReaperRecorder()
+    def _open_project(self, project_file):
+        if self.get_open_project_path() != project_file:
+            self._save_current_project()
+            reapy.open_project(project_file)
 
 
 
 def with_session(f):
     def inner(self, *args, **kwargs):
+
         session = Session.objects.filter(pk=self.session_id).first()
-        if session and self.reaper.get_open_project_path() == session.project_file:
+        if session:
             args = args + (session,)
             result = f(self, *args, **kwargs)
-            appState.update('UPDATE_FULL_SESSION', session.to_dict())
+            self.update_module_status()
             return result
         else:
             log.warning("No active session, did not execute %s" % f.__name__)
+
     return inner
 
 
+class SessionRecorder(StateModule):
+    name = 'UPDATE_FULL_SESSION'
 
-class SessionRecorder():
-    def __init__(self):
+    def __init__(self, reaper):
         self.reaper = reaper
         self.session_id = None
-        self.recorder_state = DISCONNECTED
-
-        reaper.on_connect = self.on_reaper_connected
-        reaper.on_disconnect = self.on_reaper_disconnected
-        reaper.connect()
-
-        timer = Thread(target=self.scheduled_recorder_update)
-        timer.setDaemon(True)
-        timer.start()
-
-    def scheduled_recorder_update(self):
-        while True:
-            sleep(.1)
-            if self.recorder_state != DISCONNECTED:
-                self._send_update(recorder=True, session=False)
+        self.reaper.on_connect = self.on_reaper_connected
+        self.reaper.connect()
 
     def on_reaper_connected(self):
         log.warning("Reaper connected-   - - - - - - - - -")
-        self.recorder_state = CONNECTED
         self._identfy_open_project()
 
-    def on_reaper_disconnected(self):
-        log.warning("Reaper was disconnected, will try to reconnect soon")
-        self.session_id = None
-        self.recorder_state = DISCONNECTED
-        timer = Timer(10, reaper.connect)
-        timer.setDaemon(True)
-        timer.start()
+    def get_module_status(self):
+        session = Session.objects.filter(pk=self.session_id).first()
+        if session:
+            return session.to_dict()
 
-        self._send_update(session=False)
-
+    @action
     def session_start(self, session_name):
-        session_file_location = reaper.start_new_project(session_name)
-        self._create_session(session_name, session_file_location)
-        self._send_update(recorder=True)
+        session_file_location = self.reaper.start_new_project(session_name)
+        session = Session.objects.create(name=session_name, username=CURRENT_USER,
+                                         project_file=session_file_location)
+        self.session_id = session.id
+        self.update_module_status()
 
+    @action
     @with_session
     def session_close(self, data, session):
-        reaper.close_project()
+        self.reaper.close_project()
         self.session_id = None
-        self.recorder_state = CONNECTED
-        self._send_update(recorder=True)
 
+    @action
     @with_session
     def set_next_take_name(self, take_name, session):
         session.next_take_name = take_name
         session.save()
-        self._send_update()
 
+    @action
     @with_session
     def take_start(self, data, session):
-        start_position = reaper.start_recording()
+        start_position = self.reaper.start_recording()
         self._create_take(session, start_position)
-        self._send_update(recorder=True)
 
+    @action
     @with_session
     def take_stop(self, data, session):
-        end_position = reaper.stop()
+        end_position = self.reaper.stop()
         session.active_take.length = end_position - session.active_take.location
         session.active_take.save()
-        reaper.select(session.active_take.location, session.active_take.length)
-        self._send_update(recorder=True)
+        self.reaper.select(session.active_take.location, session.active_take.length)
 
-
+    @action
     @with_session
     def take_select(self, number, session):
         take = session.takes.filter(number=number).first()
-        reaper.select(take.location, take.length)
+        self.reaper.select(take.location, take.length)
         session.active_take = take
         session.save()
-        self._send_update(recorder=True)
 
+    @action
     @with_session
     def take_seek(self, position, session):
-        reaper.seek(session.active_take.location + position)
-        self._send_update(session=False, recorder=True)
+        self.reaper.seek(session.active_take.location + position)
 
+    @action
     @with_session
     def play(self, *args):
-        reaper.play()
-        self._send_update(recorder=True)
+        self.reaper.play()
 
+    @action
     @with_session
     def stop(self, *args):
-        reaper.stop()
-        self._send_update(recorder=True)
-
-    def _send_update(self, session=True, recorder=False):
-        if recorder:
-            self.recorder_state = reaper.get_status()
-            appState.update('RECORDER_UPDATE', self.recorder_state)
+        self.reaper.stop()
 
     def _create_take(self, session, position):
         take = session.takes.create(number=session.next_take_number,
@@ -306,24 +319,35 @@ class SessionRecorder():
         session.active_take = take
         session.save()
 
-    def _create_session(self, name, session_file_host):
-        session = Session.objects.create(name=name, username=CURRENT_USER,
-                                         project_file=session_file_host)
-
-        self.session_id = session.id
-
     def _identfy_open_project(self):
         log.warning("Identigy open Project")
-        session_file = reaper.get_open_project_path()
+        session_file = self.reaper.get_open_project_path()
 
         session = Session.objects.filter(project_file=session_file).first()
         if session:
-            appState.update('UPDATE_FULL_SESSION', session.to_dict())
             self.session_id = session.id
+            self.update_module_status()
 
 
-recorder = SessionRecorder()
 
+
+
+class Authentication(StateModule):
+    name = 'ACTIVE_USER'
+
+    def get_module_status(self):
+        return CURRENT_USER
+
+
+appState = ApplicationState()
+reaper = ReaperRecorder()
+
+recorder = SessionRecorder(reaper)
+auth = Authentication()
+
+appState.register_module(auth)
+appState.register_module(recorder)
+appState.register_module(reaper)
 
 
 class RemoteConsumer(JsonWebsocketConsumer):
@@ -334,17 +358,16 @@ class RemoteConsumer(JsonWebsocketConsumer):
             self.channel_name
         )
 
-        self.send_json(mutation('ACTIVE_USER', CURRENT_USER))
-
-        self.send_update({'scope': 'UPDATE_FULL_SESSION'})
-        self.send_update({'scope': 'RECORDER_UPDATE'})
+        for status in appState.all_states():
+            self.send_json(status)
 
     def send_update(self, data):
         scope = data['scope']
-        self.send_json(mutation(scope, appState.state[scope]))
+        self.send_json(appState.get_status_message(scope))
 
     def receive_json(self, content, **kwargs):
-        getattr(recorder, content['action'])(content.get('data', None))
+        appState.dispatch_action(content['action'], content.get('data', None))
+
 
 # login / logout
 # session_start / session_close
